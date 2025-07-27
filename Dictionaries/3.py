9808c50342f413893ed118af9592fde116ce34e2
Abdullah <as37755488@gmail.com>
2025-07-27 18:56:52 +0300

    Add the third program in dictionaries

---
+++ Dictionaries/3.py
@@ -0,0 +1,12 @@
+def main():
+    dictionary = {
+        "test": "c",
+        "example": "a",
+        "sample": "b",
+    }
+
+    for key, value in dictionary.items():
+        print(f"{key}: {value}")
+
+if __name__ == "__main__":
+    main()
