6cf74b3131a57fc3cc5c2cadeaca317a44ee470b
Abdullah <as37755488@gmail.com>
2025-07-27 15:36:29 +0300

    Add the third program in basics

---
+++ Basics/3.py
@@ -0,0 +1,23 @@
+from math import pi
+
+def main():
+    radius = get_radius()
+    volume = get_volume(radius)
+    print(f"The volume of the sphere with radius {radius} is: {volume:.2f}")
+
+
+def get_volume(radius):
+    return 4 / 3 * pi * radius ** 3
+
+def get_radius():
+    while True:
+        radius = input("Enter the radius: ")
+
+        if radius.isdigit():
+            return float(radius)
+        
+        print("Invalid input. Please enter a valid number.")
+
+
+if __name__ == "__main__":
+    main()
