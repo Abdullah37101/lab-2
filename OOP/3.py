1964a9177ed9e8032205922563a3df831ca12f37
Abdullah <as37755488@gmail.com>
2025-07-27 19:14:31 +0300

    Add the third program in OOP

---
+++ OOP/3.py
@@ -0,0 +1,25 @@
+class Calculator:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+    @staticmethod
+    def multiply(a, b):
+        return a * b
+
+    @staticmethod
+    def divide(a, b):
+        if b == 0:
+            raise ValueError("Cannot divide by zero")
+        return a / b
+
+def main():
+    print(f"5 / 2 = {Calculator.divide(5, 2)}")
+
+
+if __name__ == "__main__":
+    main()
